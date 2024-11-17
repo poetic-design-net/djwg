3085e15c55e3107c7af38fa97466f1cc272fc36a
poetic-design-net <hallo@frdrk.de>
2024-11-17 19:52:47 +0100

    chore: Add vercel.json and update package.json for Vercel deployment

--- app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/0.js
+++ app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/0.js
@@ -8,6 +8,6 @@ export { universal };
 export const universal_id = "src/routes/+layout.ts";
 export { server };
 export const server_id = "src/routes/+layout.server.ts";
-export const imports = ["_app/immutable/nodes/0.DOkgrNJQ.js","_app/immutable/chunks/0.7b_ZNbxr.js","_app/immutable/chunks/index.CCg6-XlO.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/if.D9LSaWDq.js","_app/immutable/chunks/client.C1mLzEnz.js","_app/immutable/chunks/preload-helper.BqZSpHe9.js","_app/immutable/chunks/attributes.CJjV9I4q.js","_app/immutable/chunks/stores.CdskA7_W.js","_app/immutable/chunks/entry.bWnWDS-E.js","_app/immutable/chunks/class.Bcz9j7d2.js"];
+export const imports = ["_app/immutable/nodes/0.DjMRJhxE.js","_app/immutable/chunks/0.Ccx9_IM-.js","_app/immutable/chunks/index.CCg6-XlO.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/if.D9LSaWDq.js","_app/immutable/chunks/client.C1mLzEnz.js","_app/immutable/chunks/preload-helper.BqZSpHe9.js","_app/immutable/chunks/attributes.CJjV9I4q.js","_app/immutable/chunks/stores.Dc01G7wV.js","_app/immutable/chunks/entry.B4MxmJkG.js","_app/immutable/chunks/class.Bcz9j7d2.js"];
 export const stylesheets = ["_app/immutable/assets/0.zmDvmCVh.css"];
 export const fonts = [];
--- app/.vercel/output/static/_app/immutable/entry/start.D8lgZMiZ.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"../chunks/entry.bWnWDS-E.js";export{a as start};
