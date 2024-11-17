c2537796fe393ab5424e116e3efceb1ade22a49d
poetic-design-net <hallo@frdrk.de>
2024-11-17 19:56:55 +0100

    fix: Move build dependencies to dependencies section for Vercel

--- app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BCWmAJVW.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/render.BD8I6o9n.js","_app/immutable/chunks/stores.Dc01G7wV.js","_app/immutable/chunks/entry.B4MxmJkG.js","_app/immutable/chunks/index.CCg6-XlO.js"];
-export const stylesheets = [];
-export const fonts = [];
--- app/.vercel/output/static/_app/immutable/chunks/svelte-component.B153s9Q5.js
+++
@@ -1,1 +0,0 @@
-import{d as c,E as i,g as s,h as r,i as h,j as d,k as p}from"./index-client.Bm3hC5iA.js";function u(t,f,o){r&&h();var n=t,a,e;c(()=>{a!==(a=f())&&(e&&(d(e),e=null),a&&(e=s(()=>o(n,a))))},i),r&&(n=p)}export{u as c};
