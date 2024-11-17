628263bc02ad8e1155a8e5b594100b1e3b7c9dbf
poetic-design-net <hallo@frdrk.de>
2024-11-17 19:52:47 +0100

    chore: Add vercel.json and update package.json for Vercel deployment

--- app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/1.js
+++ app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.1JlfNBbt.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/render.BD8I6o9n.js","_app/immutable/chunks/stores.CdskA7_W.js","_app/immutable/chunks/entry.bWnWDS-E.js","_app/immutable/chunks/index.CCg6-XlO.js"];
+export const imports = ["_app/immutable/nodes/1.BCWmAJVW.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/render.BD8I6o9n.js","_app/immutable/chunks/stores.Dc01G7wV.js","_app/immutable/chunks/entry.B4MxmJkG.js","_app/immutable/chunks/index.CCg6-XlO.js"];
 export const stylesheets = [];
 export const fonts = [];
--- app/.vercel/output/static/_app/immutable/nodes/0.DOkgrNJQ.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/0.7b_ZNbxr.js";export{e as component,n as universal};
