1cca08fda42e3ed28a7175dc55770ec2432b5c30
poetic-design-net <hallo@frdrk.de>
2024-11-17 22:39:36 +0100

    fix hopefully netifly

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,8 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		'process.env': process.env
-	  }
+  plugins: [sveltekit()],
+  base: '/',
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.js', '.ts', '.svelte']
+  }
 });
