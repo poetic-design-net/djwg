c892d73d54562af4570382d3e19e47619cfa5a53
poetic-design-net <hallo@frdrk.de>
2024-11-17 20:00:55 +0100

    fix: Move build dependencies to dependencies section for Vercel

---
+++ app/.vercel/output/functions/fn.func/app/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.uOjrRtcN.js","_app/immutable/chunks/disclose-version.BbHg_3cr.js","_app/immutable/chunks/index-client.Bm3hC5iA.js","_app/immutable/chunks/legacy.4_-7NDyu.js","_app/immutable/chunks/render.BD8I6o9n.js","_app/immutable/chunks/stores.aYYyu5_J.js","_app/immutable/chunks/entry.B6U6h_cC.js","_app/immutable/chunks/index.CCg6-XlO.js"];
+export const stylesheets = [];
+export const fonts = [];
---
+++ app/.vercel/output/static/_app/immutable/entry/start.CiuKsm7s.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.B6U6h_cC.js";export{a as start};
