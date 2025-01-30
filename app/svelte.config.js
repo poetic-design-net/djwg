e7e3d6a5c0ddf9877c0e73fe758453032fc0253c
poetic-design-net <hallo@frdrk.de>
2025-01-30 10:46:36 +0100

    update shedule view

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// Use Node.js 20 runtime
-			runtime: 'edge'
+			runtime: 'nodejs20.x'
 		})
 	}
 };
