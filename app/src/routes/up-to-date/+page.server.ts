37432357ff486644564ba139a64b6f266e33f304
poetic-design-net <hallo@frdrk.de>
2025-01-30 10:46:36 +0100

    update shedule view

--- app/src/routes/up-to-date/+page.server.ts
+++ app/src/routes/up-to-date/+page.server.ts
@@ -1,9 +1,9 @@
-import { getSocialMediaLinks } from '$lib/sanity/queries/settings'
+import { siteSettingsQuery } from '$lib/sanity/queries/settings'
 import { client } from '$lib/sanity/client'
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async () => {
-  const data = await client.fetch(getSocialMediaLinks)
+  const data = await client.fetch(siteSettingsQuery)
   return {
     socialMedia: data?.socialMedia || []
   }
