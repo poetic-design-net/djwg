1b299746f3d7706016e9f0cf8929b5f5671c07ef
poetic-design-net <hallo@frdrk.de>
2025-02-12 15:16:45 +0100

    add sentry

--- app/src/hooks.client.ts
+++ app/src/hooks.client.ts
@@ -1,7 +1,18 @@
+import * as Sentry from '@sentry/sveltekit';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import type { Handle } from '@sveltejs/kit';
 import { invalidate } from '$app/navigation';
+
+// If you don't want to use Session Replay, remove the `Replay` integration,
+// `replaysSessionSampleRate` and `replaysOnErrorSampleRate` options.
+Sentry.init({
+    dsn: "https://2ce9830d7f9b44fb02f10e6a023a1d8e@o4508806453526528.ingest.de.sentry.io/4508806537609296",
+    tracesSampleRate: 1,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1,
+    integrations: [Sentry.replayIntegration()]
+})
 
 export const handle: Handle = async ({ event, resolve }) => {
   try {
@@ -56,3 +67,4 @@ export const handle: Handle = async ({ event, resolve }) => {
     });
   }
 };
+export const handleError = Sentry.handleErrorWithSentry();
