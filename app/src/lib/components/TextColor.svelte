284a9de8d0fb88703b74f7abfbe1dd7311ee17a0
poetic-design-net <hallo@frdrk.de>
2025-04-01 17:01:01 +0200

    add color to portable

---
+++ app/src/lib/components/TextColor.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let value: { value: string };
+  export let children: any;
+</script>
+
+<span style="color: {value.value}">{children}</span>
---
+++ app/src/lib/components/portable-text/HighlightColor.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let portableText: {
+    value: { value: string };
+    plainTextContent: string;
+  };
+</script>
+
+<span style="background-color: {portableText.value.value}">{portableText.plainTextContent}</span>
